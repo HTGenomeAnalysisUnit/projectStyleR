0d91befc54f50c2058494b0070ffc161692bf6d7
edoardo.giacopuzzi <edoardo.giacopuzzi@fht.org>
2025-09-04 10:22:16 +0200

    fix palettes.yaml filename, return palette content in display_project_palette

--- R/palettes.R
+++ R/palettes.R
@@ -1,14 +1,16 @@
 #' Display a project color palette
 #'
 #' Creates a simple ggplot visualization of a specified, loaded color palette.
+#' Returns the palette as a named vector of colors or vector of colors depending on the palette structure.
 #'
 #' @param palette_name The name of the palette to display (e.g., "primary", "vibrant").
-#' @return A ggplot object visualizing the color palette.
+#' @param show Logical, if TRUE (default) will display the palette using ggplot2.
+#' @return A named vector of colors if the palette has names, otherwise a vector of colors.
 #' @export
 #' @examples
 #' # This will display the default 'primary' palette on package load
 #' display_project_palette("primary")
-display_project_palette <- function(palette_name) {
+display_project_palette <- function(palette_name, show=TRUE) {
   project_palettes <- get_project_palettes() # Use the new helper function
   
   if (!palette_name %in% names(project_palettes)) {
@@ -16,20 +18,25 @@ display_project_palette <- function(palette_name) {
   }
 
   pal <- project_palettes[[palette_name]]
-  pal_df <- data.frame(
-    name = names(pal),
-    color = unlist(unname(pal)),
-    stringsAsFactors = FALSE
-  )
-  
-  pal_df$name <- factor(pal_df$name, levels = names(pal))
-  
-  ggplot2::ggplot(pal_df, ggplot2::aes(x = name, y = 1, fill = color)) +
-    ggplot2::geom_col(width = 1) +
-    ggplot2::geom_text(ggplot2::aes(label = color), vjust = 1.5, color = "white", size = 3.5, fontface = "bold") +
-    ggplot2::geom_text(ggplot2::aes(label = names(pal)), vjust = -0.5, color = "black", size = 3.5, angle = 45) +
-    ggplot2::scale_fill_identity() +
-    ggplot2::labs(title = paste("Project Palette:", palette_name)) +
-    ggplot2::theme_void() +
-    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+
+  if (show) {
+    pal_df <- data.frame(
+      name = names(pal),
+      color = unlist(unname(pal)),
+      stringsAsFactors = FALSE
+    )
+    
+    pal_df$name <- factor(pal_df$name, levels = names(pal))
+    
+    ggplot2::ggplot(pal_df, ggplot2::aes(x = name, y = 1, fill = color)) +
+      ggplot2::geom_col(width = 1) +
+      ggplot2::geom_text(ggplot2::aes(label = color), vjust = 1.5, color = "white", size = 3.5, fontface = "bold") +
+      ggplot2::geom_text(ggplot2::aes(label = names(pal)), vjust = -0.5, color = "black", size = 3.5, angle = 45) +
+      ggplot2::scale_fill_identity() +
+      ggplot2::labs(title = paste("Project Palette:", palette_name)) +
+      ggplot2::theme_void() +
+      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+  }
+
+  return(pal)
 }
